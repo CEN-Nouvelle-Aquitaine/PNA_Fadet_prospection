0ed06ebfd7c1a5858a2fa0445d7ebd5f61e56d80
Wanderzen <103043914+wanderzen91@users.noreply.github.com>
2023-03-14 14:25:24 +0100

    Add files via upload

---
+++ global.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(leaflet)
+library(DBI)
+library(RPostgreSQL)
+library(RPostgres)
+library(leaflet)
+library(shinyWidgets)
+library(DT)
+library(glue)
+library(shinycssloaders)
+library(shinyalert)
+library(shinyjs)
+library(shinydashboard)
+library(pool)
+library(RColorBrewer)
+library(sf)
+library(tidyr)
+library(dplyr)
+
+# setwd("C:/Users/Romain/Downloads/PNA_Papillons")
+
+
+region_NA <- st_read("departement.geojson")
+
+
+
+js <- '
+$(document).on("shiny:connected", function(){
+  $("#map").css({
+    width: window.innerWidth, 
+    height: window.innerHeight
+  });
+  $(window).on("resize", function(e){
+    if(e.target instanceof Window){
+      $("#map").css({width: window.innerWidth, height: window.innerHeight});
+    }
+  });
+})'
+
